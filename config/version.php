684f35157c7882d1752c56ec0d2498a55cba8e41
cyxSetyo <cahyasetya003@gmail.com>
2023-12-05 16:27:44 +0700

    Initial commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.2.1',
+  'full_app_version' => 'v6.2.1 - build 11625-gc45ede2d1',
+  'build_version' => '11625',
+  'prerelease_version' => '',
+  'hash_version' => 'gc45ede2d1',
+  'full_hash' => 'v6.2.1-47-gc45ede2d1',
+  'branch' => 'master',
+);
